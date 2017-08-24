05b09ee9ffd6568b9bdc747e84cf94fbf5ddc0b2
kethan92 <kethan92@gmail.com>
2017-08-24 18:00:57 +0700

    Add project files.

---
+++ HotelManagement_ClientWithAngularJS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HotelManagement_ClientWithAngularJS.MvcApplication" Language="C#" %>
---
+++ HotelManagement_ClientWithAngularJS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HotelManagement_ClientWithAngularJS.Startup))]
+namespace HotelManagement_ClientWithAngularJS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
